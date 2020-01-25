ed059409a133cc66246be23e905b2734cca8bab6
TurkishKebab <mario.turco4@studenti.unina.it>
2020-01-25 12:49:25 +0100

    Aggiunto parser

--- Test/prova.c
+++ Test/prova.c
@@ -1,73 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#define ROWS 3
-#define COLUMNS 4
+#include"parser.h"
+#include<stdio.h>
 
-void printGrid(char grid[][COLUMNS]);
-void update(int, int, char grid[][COLUMNS]);
-void fillGrid(int, int, char grid[][COLUMNS]);
 
-int main(int argc, char *argv[]) {
-  char grid[ROWS][COLUMNS];
-  int numeroColonna = 0, numeroRiga = 2;
-  char movement;
-  system("clear");
-  fillGrid(0, 0, grid);
-  printGrid(grid);
-  while (1) {
-    movement = getchar();
-    switch (movement) {
-    case 'w':
-      numeroRiga--;
-      numeroRiga = numeroRiga % ROWS;
-      update(numeroColonna, numeroRiga, grid);
-      break;
+int main(int argc, char**argv){
+    appendPlayer("azankiew","machevuoi98",argv[1]);
+    appendPlayer("basta","s",argv[1]);
+    appendPlayer("vabe","e",argv[1]);
+    appendPlayer("boi","rr",argv[1]);
+    appendPlayer("ew","t",argv[1]);
+    appendPlayer("ao","g",argv[1]);
+    printf("%d\n", isRegistered("boi",argv[1]));
+    printf("%d\n", isRegistered("ao",argv[1]));
+    printf("%d\n", isRegistered("azankiew",argv[1]));
+    printf("%d\n", isRegistered("mamt",argv[1]));
+    printf("L'utente azankiew è loggato? %d\n",validateLogin("azankiew","machevuoi98",argv[1]));//si
+    printf("L'utente aeEeEeEeE è loggato? %d\n",validateLogin("aeEeEeEeE","machevuoi98",argv[1]));//non registrato ancora
+    printf("L'utente azankiew è loggato? %d\n",validateLogin("azankiew","machevuoi",argv[1]));//credenziali errate ma registrato
 
-    case 's':
-      numeroRiga++;
-      numeroRiga = numeroRiga % ROWS;
-      update(numeroColonna, numeroRiga, grid);
-      break;
-
-    case 'a':
-      numeroColonna--;
-      numeroColonna = numeroColonna % COLUMNS;
-      update(numeroColonna, numeroRiga, grid);
-      break;
-    case 'd':
-      numeroColonna++;
-      numeroColonna = numeroColonna % COLUMNS;
-      update(numeroColonna, numeroRiga, grid);
-      break;
-    default:
-      break;
-    }
-  }
-  return 0;
-}
-void update(int numeroColonna, int numeroRiga, char grid[][COLUMNS]) {
-  fillGrid(numeroRiga, numeroColonna, grid);
-  system("clear");
-  printGrid(grid);
-}
-void fillGrid(int numeroRiga, int numeroColonna, char grid[][COLUMNS]) {
-  int i = 0, j = 0;
-  char a = 'a';
-  for (i = 0; i < ROWS; i++) {
-    for (j = 0; j < COLUMNS; j++) {
-      grid[i][j] = a++;
-    }
-  }
-  grid[numeroColonna][numeroRiga] = '@';
-}
-
-void printGrid(char grigliaDaStampare[ROWS][COLUMNS]) {
-  int i = 0, j = 0;
-  for (i = 0; i < ROWS; i++) {
-    for (j = 0; j < COLUMNS; j++) {
-      printf("%c", grigliaDaStampare[i][j]);
-    }
-    printf("\n");
-  }
+    return 0;
 }
---
+++ parser.h
@@ -0,0 +1,5 @@
+int appendPlayer(char* name,char* pwd,char* file);
+int isRegistered(char* name,char* file);
+int openFileRDWRAPP(char* file);
+int validateLogin(char* name,char* pwd,char* file);
+int openFileRDON(char* file);

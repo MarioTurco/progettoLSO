ebca9a06eac184439293ee25230573f8f85df9ef
TurkishKebab <mario.turco4@studenti.unina.it>
2020-01-31 12:47:20 +0100

    Aggiunto controllo ostacolo

--- boardUtility.c
+++ boardUtility.c
@@ -37,9 +37,11 @@ int colpitoPacco(char grigliaDiGioco[ROWS][COLUMNS], int posizione[2]) {
     return 1;
   return 0;
 }
-int spostamentoValid(char grigliaDiGioco[ROWS][COLUMNS], int posizione[2]) {
-  if (grigliaDiGioco[posizione[0]][posizione[1]] == '$')
-    return 1;
+int spostamentoValido(char grigliaDiGioco[ROWS][COLUMNS],
+                      char grigliaOstacoli[ROWS][COLUMNS], int posizione[2]) {
+  if (grigliaDiGioco[posizione[0]][posizione[1]] == '-')
+    if (grigliaOstacoli[posizione[0]][posizione[1]] == '-')
+      return 1;
   return 0;
 }
 int colpitoPlayer(char grigliaDiGioco[ROWS][COLUMNS], int posizione[2]) {
--- boardUtility.h
+++ boardUtility.h
@@ -39,3 +39,5 @@ void mergeGridAndList(char grid[ROWS][COLUMNS], Obstacles top);
 int colpitoOstacolo(char grigliaOstacoli[ROWS][COLUMNS], int posizione[2]);
 int colpitoPacco(char grigliaDiGioco[ROWS][COLUMNS], int posizione[2]);
 int colpitoPlayer(char grigliaDiGioco[ROWS][COLUMNS], int posizione[2]);
+int spostamentoValido(char grigliaDiGioco[ROWS][COLUMNS],
+                      char grigliaOstacoli[ROWS][COLUMNS], int posizione[2]);
